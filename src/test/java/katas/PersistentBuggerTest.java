9d7dfc728a5b287fa2c61e42ff6eebee75ca5611
Vyacheslav Batkov <vbatkov@ec-leasing.ru>
2017-11-28 17:09:11 +0300

    [Get the Middle Character] solved

---
+++ src/test/java/katas/PersistentBuggerTest.java
@@ -0,0 +1,22 @@
+package katas;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PersistentBuggerTest {
+    @Test
+    public void persistence1() throws Exception {
+        assertEquals(PersistentBugger.persistence(39), 3);
+    }
+
+    @Test
+    public void persistence2() throws Exception {
+        assertEquals(PersistentBugger.persistence(999), 4);
+    }
+
+    @Test
+    public void persistence3() throws Exception {
+        assertEquals(PersistentBugger.persistence(4), 0);
+    }
+}

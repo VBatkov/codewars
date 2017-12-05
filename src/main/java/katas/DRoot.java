19134e1253b0853c1c5de012fb164b78796c9589
Vyacheslav Batkov <vbatkov@ec-leasing.ru>
2017-12-05 11:35:19 +0300

    [Sum of Digits / Digital Root] solved

---
+++ src/main/java/katas/DRoot.java
@@ -0,0 +1,52 @@
+package katas;
+
+/**
+ * 6 kyu
+ * <p>
+ * Sum of Digits / Digital Root
+ * <p>
+ * In this kata, you must create a digital root function.
+ * <p>
+ * A digital root is the recursive sum of all the digits in a number. Given n, take the sum of the digits of n.
+ * If that value has two digits, continue reducing in this way until a single-digit number is produced.
+ * This is only applicable to the natural numbers.
+ * <p>
+ * Here's how it works (Ruby example given):
+ * <p>
+ * digital_root(16)
+ * => 1 + 6
+ * => 7
+ * <p>
+ * digital_root(942)
+ * => 9 + 4 + 2
+ * => 15 ...
+ * => 1 + 5
+ * => 6
+ * <p>
+ * digital_root(132189)
+ * => 1 + 3 + 2 + 1 + 8 + 9
+ * => 24 ...
+ * => 2 + 4
+ * => 6
+ * <p>
+ * digital_root(493193)
+ * => 4 + 9 + 3 + 1 + 9 + 3
+ * => 29 ...
+ * => 2 + 9
+ * => 11 ...
+ * => 1 + 1
+ * => 2
+ */
+class DRoot {
+    static int digital_root(int n) {
+        if (n < 10) return 0;
+        int sum = String.valueOf(n).chars().mapToObj(i -> (char)i).mapToInt(Character::getNumericValue).sum();
+        if (sum >= 10)
+            return digital_root(sum);
+        else
+            return sum;
+
+        // Альтернативное решение в одну строку (не моё, но чертовски красивое)
+        // return (n != 0 && n%9 == 0) ? 9 : n % 9;
+    }
+}
---
+++ src/test/java/katas/DRootTest.java
@@ -0,0 +1,37 @@
+package katas;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DRootTest {
+    @Test
+    public void digital_root1() throws Exception {
+        assertEquals("Nope!", DRoot.digital_root(16), 7);
+    }
+
+    @Test
+    public void digital_root2() throws Exception {
+        assertEquals("Nope!", DRoot.digital_root(942), 6);
+    }
+
+    @Test
+    public void digital_root3() throws Exception {
+        assertEquals("Nope!", DRoot.digital_root(132189), 6);
+    }
+
+    @Test
+    public void digital_root4() throws Exception {
+        assertEquals("Nope!", DRoot.digital_root(493193), 2);
+    }
+
+    @Test
+    public void digital_root5() throws Exception {
+        assertEquals("Nope!", DRoot.digital_root(10), 1);
+    }
+
+    @Test
+    public void digital_root6() throws Exception {
+        assertEquals("Nope!", DRoot.digital_root(100009), 1);
+    }
+}
